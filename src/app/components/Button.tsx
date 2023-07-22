f34efa9739465c902b5c55f590a61ca2c9da8f05
johanes <johannesseleshi@gmail.com>
2023-07-22 08:09:00 -0400

    'first commit'

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,33 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { ButtonHTMLAttributes, FunctionComponent } from "react";
+
+const buttonVarirants = cva(
+    "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+    {
+        variants: {
+            varirant: {
+                default: "bg-red-900 text-white hover:bg-red-800 ",
+                ghost: "bg-transparent hover:text-red-900 hover:text-red-200",
+            },
+            size: {
+                default: "h-10 py-2 px-4",
+                sm: "h-9 px-2",
+                lg: "h-11 px-8",
+            },
+        },
+        defaultVariants: {
+            varirant: "default",
+            size: "default",
+        },
+    }
+);
+
+export interface ButtonProps
+    extends ButtonHTMLAttributes<HTMLButtonElement>,
+        VariantProps<typeof buttonVarirants> {}
+
+const Button: FunctionComponent<ButtonProps> = ({}) => {
+    return <Button />;
+};
+
+export default Button;
