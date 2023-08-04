b375b6d395c664e7f16a59c5fe19e4a430743f0a
johanes <johannesseleshi@gmail.com>
2023-08-04 00:14:24 -0400

    err with realtime chat feature/realtime add works

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { addFriendValidator } from "@/lib/validations/add-friend";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
@@ -57,6 +59,17 @@ export async function POST(req: Request) {
         }
 
         // valid request, send friend request
+
+        console.log("trigger pusher");
+
+        pusherServer.trigger(
+            toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+            "incoming_friend_requests",
+            {
+                senderId: session.user.id,
+                senderEmail: session.user.email,
+            }
+        );
 
         await db.sadd(
             `user:${idToAdd}:incoming_friend_requests`,
